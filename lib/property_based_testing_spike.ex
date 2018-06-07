bf9ebe4b5420b5f6bf15fb47ed5b6d4cb79583d4
Sergei Matheson <sergei.matheson@rea-group.com>
2018-06-07 19:51:17 +1000

    add_stuff with property test

--- lib/property_based_testing_spike.ex
+++ lib/property_based_testing_spike.ex
@@ -16,7 +16,15 @@ defmodule PropertyBasedTestingSpike do
     :world
   end
 
-  def add_two(value) do
+  def add_stuff(value) when value >= 100 do
+    value + 4
+  end
+
+  def add_stuff(value) when value < 0 do
+    value - 2
+  end
+
+  def add_stuff(value) do
     value + 2
   end
 end
--- test/property_based_testing_spike_test.exs
+++ test/property_based_testing_spike_test.exs
@@ -7,8 +7,24 @@ defmodule PropertyBasedTestingSpikeTest do
     assert PropertyBasedTestingSpike.hello() == :world
   end
 
-  test "add_two/1 manual test" do
-    assert PropertyBasedTestingSpike.add_two(2) == 4
-    refute PropertyBasedTestingSpike.add_two(1) == 4
+  test "add_stuff/1 manual test" do
+    assert PropertyBasedTestingSpike.add_stuff(2) == 4
+    assert PropertyBasedTestingSpike.add_stuff(200) == 204
+    assert PropertyBasedTestingSpike.add_stuff(-7) == -9
+    refute PropertyBasedTestingSpike.add_stuff(1) == 4
+  end
+
+  test "add_stuff/1 property-based test" do
+    check all value <- StreamData.integer(), value = abs(value), value < 100 do
+      assert value + 2 == PropertyBasedTestingSpike.add_stuff(value)
+    end
+
+    check all value <- StreamData.integer(), value = abs(value) + 100 do
+      assert value + 4 == PropertyBasedTestingSpike.add_stuff(value)
+    end
+
+    check all value <- StreamData.integer(), value = 0 - abs(value), value < 0 do
+      assert value - 2 == PropertyBasedTestingSpike.add_stuff(value)
+    end
   end
 end
